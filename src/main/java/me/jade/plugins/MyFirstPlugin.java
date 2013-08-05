fb558784878d02292fb3dcb96051e049b66fe2e6
Jade <darky@darky1126.com>
2013-08-05 04:29:40 -0600

    Removed iJ stuffs

--- src/main/java/me/jade/plugins/MyFirstPlugin.java
+++ src/main/java/me/jade/plugins/MyFirstPlugin.java
@@ -2,13 +2,6 @@ package me.jade.plugins;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Created with IntelliJ IDEA.
- * User: test
- * Date: 8/5/13
- * Time: 4:04 AM
- * To change this template use File | Settings | File Templates.
- */
 public class MyFirstPlugin extends JavaPlugin {
        public void onEnable() {
               getLogger().info("My first plugin");
